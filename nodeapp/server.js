1eeec4e48b1dd6f231ae0b5da103d910c456c325
Shruti Bagwe <shruti@perpetualny.com>
2022-09-29 12:03:54 +0530

    added index.html

--- nodeapp/server.js
+++ nodeapp/server.js
@@ -1,17 +1,19 @@
-'use strict';
+let http = require('http');
+let fs = require('fs');
 
-const express = require('express');
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
-});
+http.createServer(handleRequest).listen(3000);
